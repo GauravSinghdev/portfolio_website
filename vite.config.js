ee4fc5c07c866bc0d96f4ff9e8fa6df8b46eef85
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-04-20 13:26:41 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  base: '/portfolio_website/',
-  
-  // -----> this is for removing error after github pages deployed successfully and to run the website.
-
-  // below line is req for github pages deployment.
-
-  build:{
-    outDir: './docs',   
-  },
 });
