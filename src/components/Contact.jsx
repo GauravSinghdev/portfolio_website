27bd709d4e2dd94c2ba04666ecfbbc57057cdb1c
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-04-21 14:20:25 +0530

    logoset

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () =>
 
                         <textarea name="message" rows="10" placeholder="Enter your message" required className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" ></textarea>
 
-                        <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Let's talk</button>
+                        <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 mb-1">Let's talk</button>
 
                     </form>
                 </div>
